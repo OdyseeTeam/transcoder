9fda8a5e58a9cc0d2ea6ee41d0f41699b02c8233
Andrey Beletsky <andrey@vr2.net>
2021-03-21 00:15:09 +0700

    Take downloadadble Init call off the pool

--- client/download.go
+++ client/download.go
@@ -2,32 +2,16 @@ package client
 
 import (
 	"github.com/lbryio/transcoder/pkg/dispatcher"
-	"github.com/lbryio/transcoder/video"
 )
 
 type downloader struct{}
 
 func (d downloader) Do(t dispatcher.Task) error {
 	dl := t.Payload.(Downloadable)
-	err := dl.Init()
-	if err != nil {
-		if err == video.ErrChannelNotEnabled || err == ErrAlreadyDownloading {
-			return nil
-		}
-		return err
-	}
-	err = dl.Download()
+	err := dl.Download()
 	if err != nil && err != ErrAlreadyDownloading {
 		return err
 	}
-	// for p := range dl.Progress() {
-	// 	if p.Stage == DownloadDone {
-	// 		break
-	// 	}
-	// 	if p.Error != nil {
-	// 		return err
-	// 	}
-	// }
 	return nil
 }
 
